eab3fbf6a21330e43707c771d52b67dad5f5516a
Ariel <dawei4102@gmail.com>
2022-12-06 16:19:40 +0800

    1206

--- cafe.js
+++ cafe.js
@@ -4,7 +4,7 @@ export let cafes = []
 export const updateCafes = async () => {
   cafes.push('aaa')
   try {
-    const { data } = await axios.get('https://cafenomad.tw/api/v1.2/cafes/taipei')
+    const { data } = await axios.get('https://cafenomad.tw/api/v1.2/cafes')
     cafes = data
     console.log('取得資料')
   } catch (error) {
